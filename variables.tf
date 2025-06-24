806167d9663c7678a6e2edca7f11151ca0501df8
Siwoww <simone1ft93@gmail.com>
2025-06-24 14:19:30 +0200

    commit

--- variables.tf
+++ variables.tf
@@ -2,6 +2,13 @@ variable "region" {
   default = "eu-central-1"
 }
 
+variable "vpc_cidr" {
+  default = {
+    develop = "10.100.1.0/16"
+    prod = "10.200.1.0/16"
+  }
+}
+
 #DEVELOP
 variable "develop-ingress-cidr-block" {
   default = "0.0.0.0/0"
