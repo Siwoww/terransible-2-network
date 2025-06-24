7a971c550e5614a25717ec76042a13f9c617544c
Siwoww <simone1ft93@gmail.com>
2025-06-24 14:33:25 +0200

    commit

--- providers.tf
+++ providers.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "siwoww-temporary-bucket-name"
+    key = "network/terraform.tfstate"
+    region = var.region
+  }
+}
+
 provider "aws" {
   region = var.region
 }
